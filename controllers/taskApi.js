15466a3390c42e4ee0b31229e5a0008aa34d5270
varun-cube <varun.chaudhari@cubehighways.com>
2024-09-09 18:30:46 +0530

    fixed proper stutus code

--- controllers/taskApi.js
+++ controllers/taskApi.js
@@ -2,44 +2,48 @@ const tasks = require('../task.json').tasks;
 
 const getTasksByPriority = async (req, res) => {
     const priorityTasks = tasks.filter(task => task.priority === req.params.level);
-     return res.status(200).json({ task: priorityTasks });
- };
+
+    if (!priorityTasks.length) {
+        return res.status(404).json({ err: 'No tasks found for this priority level' });
+    }
+
+    return res.status(200).json({ task: priorityTasks });
+};
 
 const getTaskById = async (req, res) => {
     const ind = getIndex(req.params.id);
     if (ind === -1) {
-        res.status(401);
-        return res.json({err: 'Id Not Found!'})
+        return res.status(404).json({ err: 'Task not found' }); // 404 for Not Found
     }
 
     res.status(200);
-    return res.json({ task: tasks[ind]});
+    return res.json({ task: tasks[ind] });
 };
 
 const getAllTasks = async (req, res) => {
     res.status(200);
-    return res.json({ tasks: tasks});
+    return res.json({ tasks: tasks });
 };
 
 const getTask = async (req, res) => {
     const completionStatus = req.query.status;
-    let filteredTasks = tasks.filter(t => String(t.completed) === completionStatus); 
+    let filteredTasks = tasks.filter(t => String(t.completed) === completionStatus);
     filteredTasks.sort((a, b) => new Date(b.creationDate) - new Date(a.creationDate));
-    
+
     res.status(200);
-    return res.json({ tasks: filteredTasks});
+    return res.json({ tasks: filteredTasks });
 }
 
-const createTask = async(req, res) => {
+const createTask = async (req, res) => {
     // If any key is missing in the req body, by default assigning empty string  
-    const { title= "", description= "", completed= ""} = req.body;
+    const { title = "", description = "", completed = "" } = req.body;
 
     if (!(title && title.length)) {
-        res.status(401);
+        res.status(400); // Bad Request
         return res.json({ err: 'Title Should Not Be Empty' });
     }
     if (!(description && description.length)) {
-        res.status(401);
+        res.status(400);
         return res.json({ err: 'Description Should Not Be Empty' });
     }
 
@@ -52,20 +56,18 @@ const createTask = async(req, res) => {
         createdAt: new Date()
     });
 
-    res.status(200);
-    return res.json({ id: newId });
+    return res.status(201).json({ id: newId }); // 201 Resource Created
 };
 
 const setPriority = async (req, res) => {
     const ind = getIndex(req.query.id);
-    if(ind === -1) {
-        res.status(400);
-        return res.json({ err: 'Id Not Found'});
+    if (ind === -1) {
+        res.status(404);
+        return res.json({ err: 'Id Not Found' });
     }
     tasks[ind].priority = req.query.priority;
 
-    res.status(200);
-    return res.json({ task: tasks[ind]});
+    return res.status(200).json({ task: tasks[ind] });
 };
 
 const updateTask = async (req, res) => {
@@ -76,29 +78,25 @@ const updateTask = async (req, res) => {
     }
     const reqBody = req.body;
     // overrides existsing data with updated data;
-    const obj = { ...tasks[ind], ...reqBody }; 
+    const obj = { ...tasks[ind], ...reqBody };
     tasks[ind] = obj;
 
-    res.status(200);
-    return res.json({ id: tasks[ind].id});
+    return res.status(200).json({ id: tasks[ind].id });
 };
 
 
-const deleteTask = async(req,res) => {
+const deleteTask = async (req, res) => {
     const ind = getIndex(req.params.id);
     if (ind === -1) {
-        res.status(401);
-        return res.json({err: 'Requested Id Not Found!'})
+        return res.status(404).json({ err: 'Requested Id Not Found!' })
     }
 
     tasks.splice(ind, 1);
-    res.status(200);
-    return res.json({ tasks: tasks });
+    return res.status(204).json({ tasks: tasks }); // No content, Successfull deletion
 };
 
-function getIndex (reqId) {
-    const ind = tasks.findIndex(task => task['id'] === parseInt(reqId));
-    return ind;
+function getIndex(reqId) {
+    return tasks.findIndex(task => task['id'] === parseInt(reqId));
 }
 
 module.exports = {
